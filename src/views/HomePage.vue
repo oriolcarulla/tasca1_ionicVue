ce5b6b815f2bf18c227babfd913cce9eed2f7fdd
Oriol Carulla <04ocarulla@gmail.com>
2025-04-04 16:20:49 +0200

    project done

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,137 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
+  <ion-page class="mt-20">
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-list id="list">
+          <ion-item>
+            <ion-img src="vaca.webp"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Vaca</h2>
+              <p>MUUUUUUUUUH</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('vaca-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="oveja.webp"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Oveja</h2>
+              <p>BEEEHHHHHH</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('oveja-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="cerdo.webp"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Cerdo</h2>
+              <p>Oing oing</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('cerdo-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="gallina.jpeg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Gallina</h2>
+              <p>quiquiriqui</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('gallina-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="pato.jpeg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Pato</h2>
+              <p>CUACK!</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('pato-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="perro.jpeg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Perro</h2>
+              <p>Guau!</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('perro-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="gato.jpg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Gato</h2>
+              <p>MIAU</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('gato-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="caballo.jpeg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Caballo</h2>
+              <p>Hiiii hiiii</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('caballo-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="burro.jpeg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Burro</h2>
+              <p>Hiaaa hiaaa</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('burro-sonido.mp3')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-img src="conejo.jpg"></ion-img>
+            <ion-label class="m-2 text-sm">
+              <h2>Conejo</h2>
+              <p>Cuix cuix</p>
+            </ion-label>
+            <ion-button slot="end" fill="outline" @click="playSound('conejo-sonido.m4a')">
+              <play-button/>
+            </ion-button>
+          </ion-item>
+        </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonPage, IonButton, IonImg  } from '@ionic/vue';
+import   playButton   from '../components/icons/playButton.vue';
+
+function playSound(audioFile: string) {
+  const audio = new Audio(audioFile);
+  audio.play();
+}
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-item{
+  display: flex;
+  justify-content: space-between;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+ion-button{
+  height: 50px;
+  width: 50px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-img {
+  width: 200px;
+  height: 200px;
 }
 </style>
